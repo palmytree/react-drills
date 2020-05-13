ac2a478a9a00cf565f3824d4517db8b976b8d7ff
palmytree <jeff@palmytree.com>
2020-05-13 15:32:46 -0500

    finished part 3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 
+      filter: '',
+      array: ['things', 'and', 'stuff', 'and', 'what', 'not']
+     }
+  }
+  updateFilter(val){
+    this.setState({
+      filter: val
+    })
+  }
+  render() { 
+    return (  
+      <div className="App">
+        <input type="text"
+        onChange={e => this.updateFilter(e.target.value)}
+        />
+        <div className="list-container">
+          {this.state.array.filter(w => w.includes(this.state.filter)).map((w, i) => <h2 key={i}>{w}</h2>)}
+        </div>
+      </div>
+    );
+  }
 }
-
+ 
 export default App;
