a1d4d18f8acaf67e83c824f73332b8e6505af697
palmytree <jeff@palmytree.com>
2020-05-13 15:19:07 -0500

    finished part1

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			text: '',
+		};
+	}
+	updateText(val) {
+		this.setState({
+			text: val,
+		});
+	}
+	render() {
+		return (
+			<div className="App">
+				<input style={{width: '50%', textAlign: 'center'}} type='text' onChange={e => this.updateText(e.target.value)} />
+				<span className='display-text'>{this.state.text}</span>
+			</div>
+		);
+	}
 }
 
 export default App;
