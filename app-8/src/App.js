19507f8ae99e6579a0b07293de017147c8f031cf
palmytree <jeff@palmytree.com>
2020-05-13 19:17:07 -0500

    finished part 8

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
+import AutoComplete from './components/AutoComplete';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			pokemon: { sprites: '' },
+			input: '',
+			options: [],
+		};
+	}
+	componentDidMount() {
+		axios.get('https://pokeapi.co/api/v2/pokemon/pikachu').then(response => {
+			this.setState({ pokemon: response.data });
+		});
+	}
+	updateNewPokemon(val) {
+		this.setState({
+			input: val,
+		});
+	}
+	async search() {
+		const { input } = this.state;
+		let res;
+		try {
+			res = await axios.get(
+				'https://pokeapi.co/api/v2/pokemon/' + input.toLowerCase()
+			);
+		} catch (err) {
+			console.error('Error response');
+			console.error(err.response.status);
+			alert('Pokemon not found, please try again');
+			this.setState({ input: '' });
+		} finally {
+			if (res) {
+				this.setState({
+					pokemon: res.data,
+					input: '',
+				});
+			}
+		}
+	}
+	render() {
+		return (
+			<div className='App'>
+				<input
+					value={this.state.input}
+					placeholder='Type to change...'
+					type='text'
+					onChange={e => this.updateNewPokemon(e.target.value)}
+				/>
+				<AutoComplete />
+				<button onClick={() => this.search()}>Submit</button>
+				<img src={this.state.pokemon.sprites.front_default} alt='' />
+				<img src={this.state.pokemon.sprites.back_default} alt='' />
+				<p>
+					<b>Name:</b> {this.state.pokemon.name}
+				</p>
+				<p>
+					<b>Number:</b> {this.state.pokemon.id}
+				</p>
+				<p>
+					<b>Height:</b> {this.state.pokemon.height}
+				</p>
+				<p>
+					<b>Weight:</b> {this.state.pokemon.weight}
+				</p>
+			</div>
+		);
+	}
 }
 
 export default App;
---
+++ app-8/src/components/AutoComplete.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class AutoComplete extends Component {
+	render() {
+		return (
+			<React.Fragment>
+				<div>
+					
+				</div>
+			</React.Fragment>
+		);
+	}
+}
+
+export default AutoComplete;
